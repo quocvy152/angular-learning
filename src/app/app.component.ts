eacb8913d3010b34dd5e2f662e2a564d4486b1c8
Phạm Quốc Vỹ <phamquocvy@Phams-Mac-mini.local>
2023-10-29 22:25:59 +0700

    Create new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { WistItem } from 'src/shared/models/wistItem';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items = [
+    new WistItem('Learn Angular'),
+    new WistItem('Get coffe', true),
+    new WistItem('Find grass that cuts itself')
+  ];
   title = 'angular-learning';
 }
---
+++ src/shared/models/wistItem.ts
@@ -0,0 +1,5 @@
+export class WistItem {
+    constructor(public wishText : string, public isComplete : boolean = false) {
+        
+    }
+}
